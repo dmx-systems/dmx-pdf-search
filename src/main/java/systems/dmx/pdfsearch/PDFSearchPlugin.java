3119e03ad37f97ad17c248dffe0c78a33866f4a7
Jörg Richter <jri@deepamehta.de>
2023-11-15 13:52:51 +0100

    Setup plugin project

---
+++ src/main/java/systems/dmx/pdfsearch/PDFSearchPlugin.java
@@ -0,0 +1,24 @@
+package systems.dmx.pdfsearch;
+
+import systems.dmx.core.osgi.PluginActivator;
+
+import java.util.logging.Logger;
+
+
+
+public class PDFSearchPlugin extends PluginActivator {
+
+    // ------------------------------------------------------------------------------------------------------- Constants
+
+    // ---------------------------------------------------------------------------------------------- Instance Variables
+
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    // -------------------------------------------------------------------------------------------------- Public Methods
+
+    // *** Hooks ***
+
+    @Override
+    public void init() {
+    }
+}
