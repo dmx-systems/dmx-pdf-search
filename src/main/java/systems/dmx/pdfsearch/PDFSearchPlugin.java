442288740a807fb92b33330ff94fbed0ef168f97
Jörg Richter <jri@deepamehta.de>
2023-11-18 17:31:15 +0100

    Setup PDFBox library
    See dmx-platform/dmx-platform#529

--- src/main/java/systems/dmx/pdfsearch/PDFSearchPlugin.java
+++ src/main/java/systems/dmx/pdfsearch/PDFSearchPlugin.java
@@ -1,24 +1,58 @@
 package systems.dmx.pdfsearch;
 
+import systems.dmx.core.ChildTopics;
+import systems.dmx.core.Topic;
 import systems.dmx.core.osgi.PluginActivator;
+import systems.dmx.core.service.Inject;
+import systems.dmx.core.service.event.PostCreateTopic;
+import static systems.dmx.files.Constants.*;
+import systems.dmx.files.FilesService;
 
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
+
+import java.io.File;
 import java.util.logging.Logger;
 
 
 
-public class PDFSearchPlugin extends PluginActivator {
+public class PDFSearchPlugin extends PluginActivator implements PostCreateTopic {
 
     // ------------------------------------------------------------------------------------------------------- Constants
 
     // ---------------------------------------------------------------------------------------------- Instance Variables
 
+    @Inject
+    private FilesService files;
+
     private Logger logger = Logger.getLogger(getClass().getName());
 
     // -------------------------------------------------------------------------------------------------- Public Methods
 
-    // *** Hooks ***
+    // *** Listeners ***
 
     @Override
-    public void init() {
+    public void postCreateTopic(Topic topic) {
+        if (topic.getTypeUri().equals(FILE)) {
+            ChildTopics ct = topic.getChildTopics();
+            String mediaType = ct.getTopic(MEDIA_TYPE).getSimpleValue().toString();
+            if (mediaType.equals("application/pdf")) {
+                String path = ct.getTopic(PATH).getSimpleValue().toString();
+                logger.info("### Indexing PDF file \"" + path + "\"");
+                File file = files.getFile(path);
+                indexPDF(file);
+            }
+        }
+    }
+
+    // ------------------------------------------------------------------------------------------------- Private Methods
+
+    private void indexPDF(File file) {
+        try {
+            PDDocument pdfDocument = Loader.loadPDF(file);
+            // TODO
+        } catch (Exception e) {
+            throw new RuntimeException("Indexing PDF failed, file=\"" + file + "\"");
+        }
     }
 }
