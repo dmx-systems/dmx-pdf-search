a126911975eaa1b503e466ebf626340689c1c9a3
Juergen Neumann <juergen@junes.eu>
2023-12-22 17:53:27 +0100

    test deploy test

--- deploy/tests/10_upload_file.sh
+++ deploy/tests/10_upload_file.sh
@@ -4,8 +4,13 @@ echo "SESSIONID=${SESSIONID}"
 echo "HOST=${HOST}"
 
 URL='access-control/user/workspace'
-WSID="$( curl -I -sS -H "Cookie: JSESSIONID=${SESSIONID}" -H "Accept: application/json" "${HOST}/${URL}" | jq .id )"
-echo "WSID=${WSID}"
+WSID="$( curl -sS -H "Cookie: JSESSIONID=${SESSIONID}" -H "Accept: application/json" "${HOST}/${URL}" | jq .id )"
+if [ -z "${WSID}" ]; then
+    echo "ERROR! Empty WSID. Upload aborted."
+    exit 1
+else
+    echo "WSID=${WSID}"
+fi
 PDF='deploy/tests/scansmpl.pdf'
 URL="upload/%2Fworkspace-${WSID}"
 echo "POST ${HOST}/${URL}"
