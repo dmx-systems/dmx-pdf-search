b23a08271f7a96ddb736bbbb2d33e52d4199ba05
Juergen Neumann <juergen@junes.eu>
2023-12-23 14:49:32 +0100

    wip: improve test

--- deploy/tests/10_upload_file.sh
+++ deploy/tests/10_upload_file.sh
@@ -19,8 +19,11 @@ URL="upload/%2Fworkspace-${WSID}"
 #echo "POST ${HOST}/${URL}"
 UPLOADED_FILENAME="$( curl -sS -H "Cookie: JSESSIONID=${SESSIONID}" -F "data=@${PDF}" "${HOST}/${URL}" | jq .fileName )"
 echo "UPLOADED_FILENAME=${UPLOADED_FILENAME}"
-if [ "${UPLOADED_FILENAME}" != "scansmpl.pdf" ]; then
+## The double quotes are important for '"scansmpl.pdf"'
+if [ "${UPLOADED_FILENAME}" != '"scansmpl.pdf"' ]; then
     echo "weird! /o\ "
 else
     echo "great! \o/ "
 fi
+## search index
+curl -sS -H "Cookie: JSESSIONID=dshc07xw2x2wrwj4j9gzhw1f" "https://dmx-pdf-search-dev.ci.dmx.systems:443//core/topics/query/facsimile" | jq .items
