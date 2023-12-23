224453cc5bdc6a03ff8861fce6bd45077e7dc197
Juergen Neumann <juergen@junes.eu>
2023-12-23 16:29:02 +0100

    wip: improve test

--- deploy/tests/10_upload_file.sh
+++ deploy/tests/10_upload_file.sh
@@ -22,14 +22,18 @@ echo "UPLOADED=${UPLOADED}"
 U_NAME="$( echo "${UPLOADED}" | jq .fileName )"
 U_ID="$( echo "${UPLOADED}" | jq .topicId )"
 
-echo "UPLOADED_FILENAME=${U_NAME}"
+# echo "UPLOADED_FILENAME=${U_NAME}"
 ## The double quotes are important for '"scansmpl.pdf"'
 if [ "${U_NAME}" != '"scansmpl.pdf"' ]; then
-    echo "weird! /o\ "
+    echo "ERROR! File upload failed."
+    echo "DEBUG: ${UPLOADED}"
+    exit 1
 else
-    echo "great! \o/ "
+    echo "INFO: PDF upload succesful. (id=${U_ID})"
 fi
 ## search index
+## DMX and tesseract have a need for sleep here to process the OCR
+sleep 3
 SEARCH_RESULT="$( curl -sS -H "Cookie: JSESSIONID=dshc07xw2x2wrwj4j9gzhw1f" "https://dmx-pdf-search-dev.ci.dmx.systems:443//core/topics/query/facsimile" )"
 echo "SEARCH_RESULT=${SEARCH_RESULT}"
 ID="$( echo "${SEARCH_RESULT}" | jq '.topics[] | select((.value | contains("scansmpl.pdf")) and (.typeUri == "dmx.files.file"))'.id)"
