76034e7a0802ca67019757836605928e8ab93a75
Juergen Neumann <juergen@junes.eu>
2023-12-23 14:23:48 +0100

    wip: improve test

--- deploy/tests/10_upload_file.sh
+++ deploy/tests/10_upload_file.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
 ## This teast uploads an image based PDF and 
-## then tests the tesseract based search result
+## then tests the tesseract based search result.
+## Some vars are imported via environment.
+##
+## jpn 20231223
 
 sleep 1
-
-echo "SESSIONID=${SESSIONID}"
-echo "HOST=${HOST}"
 
 URL='access-control/user/workspace'
 WSID="$( curl -sS -H "Cookie: JSESSIONID=${SESSIONID}" -H "Accept: application/json" "${HOST}/${URL}" | jq .id )"
 if [ -z "${WSID}" ]; then
     echo "ERROR! Empty WSID. Upload aborted."
     exit 1
-else
-    echo "WSID=${WSID}"
 fi
 PDF='deploy/tests/scansmpl.pdf'
 URL="upload/%2Fworkspace-${WSID}"
-echo "POST ${HOST}/${URL}"
-## mind "Accept" header!
-#curl -sS -H "Cookie: JSESSIONID=${SESSIONID}" -H "Accept: application/json" -d "data=@${PDF}" "${HOST}/${URL}"
-sleep 1
-UPLOADED_FILENAME="$( curl -sS -H "Cookie: JSESSIONID=${SESSIONID}" -F "data=@${PDF}" "${HOST}/${URL}" | jq .filename )"
+#echo "POST ${HOST}/${URL}"
+UPLOADED_FILENAME="$( curl -sS -H "Cookie: JSESSIONID=${SESSIONID}" -F "data=@${PDF}" "${HOST}/${URL}" )"
 echo "UPLOADED_FILENAME=${UPLOADED_FILENAME}"
