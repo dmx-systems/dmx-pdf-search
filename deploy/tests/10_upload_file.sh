78b01b20ba258d41410b370e063c56699243fbb0
Juergen Neumann <juergen@junes.eu>
2023-12-23 17:03:36 +0100

    wip: improve test

--- deploy/tests/10_upload_file.sh
+++ deploy/tests/10_upload_file.sh
@@ -33,16 +33,25 @@ else
 fi
 ## search index
 ## DMX and tesseract have a need for sleep here to process the OCR
-sleep 3
+sleep 1
 count=0
 HITS=""
 while [ -z "${HITS}" ] && [ ${count} -lt 100 ]; do
     SEARCH_RESULT="$( curl -sS -H "Cookie: JSESSIONID=${SESSIONID}" "https://dmx-pdf-search-dev.ci.dmx.systems:443//core/topics/query/facsimile" )"
-    echo "SEARCH_RESULT=${SEARCH_RESULT}"
-    HITS="$( echo "${SEARCH_RESULT}" | jq .topics[] )"
+    #echo "SEARCH_RESULT=${SEARCH_RESULT}"
+    HITS="$( echo "${SEARCH_RESULT}" | jq -c .topics[] )"
     echo -e "${count} \t HITS=${HITS}"
     count=$(( ${count} + 1 ))
-    sleep 2
+    sleep 1
 done
 ID="$( echo "${SEARCH_RESULT}" | jq '.topics[] | select((.value | contains("scansmpl.pdf")) and (.typeUri == "dmx.files.file"))'.id)"
 echo "ID=${ID}"
+if [ "${ID}" != "${U_ID}" ]; then
+    echo "ERROR! Search term 'facsimile' not found. (HITS=${HITS})"
+    exit 1
+else
+    echo "INFO: Search for 'facsimile' successful. (id=${ID})"
+fi
+
+## EOF
+
