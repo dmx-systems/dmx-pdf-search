83f130a527ea93a2ac68272f5cd0edf76ae494e2
Juergen Neumann <juergen@junes.eu>
2023-12-23 16:13:56 +0100

    wip: improve test

--- deploy/tests/10_upload_file.sh
+++ deploy/tests/10_upload_file.sh
@@ -17,19 +17,18 @@ fi
 PDF='deploy/tests/scansmpl.pdf'
 URL="upload/%2Fworkspace-${WSID}"
 #echo "POST ${HOST}/${URL}"
-UPLOADED="$( curl -sS -H "Cookie: JSESSIONID=${SESSIONID}" -F "data=@${PDF}" "${HOST}/${URL}" | jq -c . )"
+UPLOADED="$( curl -sS -H "Cookie: JSESSIONID=${SESSIONID}" -F "data=@${PDF}" "${HOST}/${URL}" | jq . )"
 echo "UPLOADED=${UPLOADED}"
-U_NAME="$( echo "${UPLOADED}" | jq -c .fileName )"
-U_ID="$( echo "${UPLOADED}" | jq -c .id )"
+U_NAME="$( echo "${UPLOADED}" | jq .fileName )"
+U_ID="$( echo "${UPLOADED}" | jq .topicId )"
 
 echo "UPLOADED_FILENAME=${U_NAME}"
 ## The double quotes are important for '"scansmpl.pdf"'
-if [ "${UPLOADED_FILENAME}" != '"scansmpl.pdf"' ]; then
+if [ "${U_NAME}" != '"scansmpl.pdf"' ]; then
     echo "weird! /o\ "
 else
     echo "great! \o/ "
 fi
 ## search index
-TOPIC_ID="$( curl -sS -H "Cookie: JSESSIONID=dshc07xw2x2wrwj4j9gzhw1f" "https://dmx-pdf-search-dev.ci.dmx.systems:443//core/topics/query/facsimile" \
-    | jq -c '.topics[] | select((.value | contains("scansmpl.pdf")) and (.typeUri == "dmx.files.file"))'.id )"
+TOPIC_ID="$( curl -sS -H "Cookie: JSESSIONID=dshc07xw2x2wrwj4j9gzhw1f" "https://dmx-pdf-search-dev.ci.dmx.systems:443//core/topics/query/facsimile" | jq '.topics[] | select((.value | contains("scansmpl.pdf")) and (.typeUri == "dmx.files.file"))'.id)"
 echo "ID=${TOPIC_ID}"
