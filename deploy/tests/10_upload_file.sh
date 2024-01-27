306a8a938a8a1db7cfc64408cc0070e96195ca7d
Juergen Neumann <juergen@junes.eu>
2024-01-27 13:12:13 +0100

    upload several pdf files

--- deploy/tests/10_upload_file.sh
+++ deploy/tests/10_upload_file.sh
@@ -8,24 +8,29 @@
 
 sleep 1
 
+declare -a PDFS=('deploy/tests/scansmpl.pdf' 'deploy/tests/true_PDF.pdf')
+
 URL='access-control/user/workspace'
 WSID="$( curl -sS -H "Cookie: JSESSIONID=${SESSIONID}" -H "Accept: application/json" "${HOST}/${URL}" | jq .id )"
 if [ -z "${WSID}" ]; then
     echo "ERROR! Empty WSID. Upload aborted."
     exit 1
 fi
-PDF='deploy/tests/scansmpl.pdf'
 URL="upload/%2Fworkspace-${WSID}"
-UPLOADED="$( curl -sS -H "Cookie: JSESSIONID=${SESSIONID}" -F "data=@${PDF}" "${HOST}/${URL}" | jq . )"
-U_NAME="$( echo "${UPLOADED}" | jq .fileName )"
-U_ID="$( echo "${UPLOADED}" | jq .topicId )"
-## The double quotes are important for '"scansmpl.pdf"'
-if [ "${U_NAME}" != '"scansmpl.pdf"' ]; then
-    echo "ERROR! File upload failed."
-    echo "DEBUG: ${UPLOADED}"
-    exit 1
-else
-    echo "INFO: File upload succesful. (id=${U_ID})"
+for pfd in ${PDFS[@]}; do
+    UPLOADED="$( curl -sS -H "Cookie: JSESSIONID=${SESSIONID}" -F "data=@${pdf}" "${HOST}/${URL}" | jq . )"
+    U_NAME="$( echo "${UPLOADED}" | jq .fileName )"
+    U_ID="$( echo "${UPLOADED}" | jq .topicId )"
+    filename="$( basename ${pdf} )"
+    quoted_filename='"'${filename}'"'
+    ## The double quotes are important for '"scansmpl.pdf"'
+    if [ "${U_NAME}" != "${quoted_filename}" ]; then
+        echo "ERROR! File upload for ${filename} failed."
+        echo "DEBUG: ${UPLOADED}"
+        exit 1
+    else
+        echo "INFO: File upload for ${filename} succesful. (id=${U_ID})"
+    fi
 fi
 
 ## EOF
